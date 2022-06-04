a2005a5406b15176ea907a20763f10fd066a4e59
caipengxiang <291458254@qq.com>
2022-06-05 07:26:40 +0800

    bugfix: add cublas header

--- caffe2/core/common_gpu.h
+++ caffe2/core/common_gpu.h
@@ -4,6 +4,17 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
+#include <cublas_v2.h>
+#include <curand.h>
+#include <driver_types.h>
+
+
+#include "caffe2/core/common.h"
+#include "caffe2/core/logging.h"
+
+#include "c10/cuda/CUDAMacros.h"
+#include "c10/cuda/CUDAMathCompat.h"
+#include <c10/cuda/CUDAGuard.h>
 // This is a macro defined for cuda fp16 support. In default, cuda fp16 is
 // supported by NVCC 7.5, but it is also included in the Tegra X1 platform with
 // a (custom?) NVCC 7.0. As a result, we would normally just check the cuda
