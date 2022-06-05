e4189dbf01f63f69bca13c652197fb0b16e34bb6
caipengxiang <291458254@qq.com>
2022-06-06 00:35:23 +0800

    add exclude function

--- cmake/public/utils.cmake
+++ cmake/public/utils.cmake
@@ -1,3 +1,12 @@
+################################################################################################
+# Exclude and prepend functionalities
+function(exclude OUTPUT INPUT)
+set(EXCLUDES ${ARGN})
+foreach(EXCLUDE ${EXCLUDES})
+        list(REMOVE_ITEM INPUT "${EXCLUDE}")
+endforeach()
+set(${OUTPUT} ${INPUT} PARENT_SCOPE)
+endfunction(exclude)
 
 ################################################################################################
 # Clears variables from list
